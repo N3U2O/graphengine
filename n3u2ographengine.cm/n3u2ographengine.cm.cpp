805c08b00b903f12f995f6bc89bfe2ffee6a5287
N3U2O <gugyin@gmail.com>
2018-09-15 14:55:19 +0200

    Add project files.

---
+++ n3u2ographengine.cm/n3u2ographengine.cm.cpp
@@ -0,0 +1,12 @@
+﻿// n3u2ographengine.cm.cpp : Defines the entry point for the application.
+//
+
+#include "n3u2ographengine.cm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ n3u2ographengine.cm/n3u2ographengine.cm.h
@@ -0,0 +1,8 @@
+﻿// n3u2ographengine.cm.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
