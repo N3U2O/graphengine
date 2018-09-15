be83ec7ecef39c67296d0c8121cf29dad44be913
N3U2O <gugyin@gmail.com>
2018-09-15 15:18:39 +0200

    http://www.rastertek.com/gl40tut02.html

---
+++ src/graphicsclass.cpp
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "graphicsclass.h"
+
+
+GraphicsClass::GraphicsClass()
+{
+}
+
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+
+bool GraphicsClass::Initialize(OpenGLClass* OpenGL, HWND hwnd)
+{
+
+	return true;
+}
+
+
+void GraphicsClass::Shutdown()
+{
+
+	return;
+}
+
+
+bool GraphicsClass::Frame()
+{
+
+	return true;
+}
+
+
+bool GraphicsClass::Render()
+{
+
+	return true;
+}
